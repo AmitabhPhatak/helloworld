734d99ef883cab7d938e7aeef98c10b2c2996563
AmitabhPhatak <47364544+AmitabhPhatak@users.noreply.github.com>
2024-03-02 00:47:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 import os
+from flask import Flask, request, jsonify, render_template
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return "Hello World!"
+def home():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
