1d74f273f05c8b7e2ca92ca43628628e4874c3a3
AmitabhPhatak <47364544+AmitabhPhatak@users.noreply.github.com>
2024-03-02 01:10:03 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,33 +1,18 @@
-import numpy as np
+from flask import Flask
+import os
 from flask import Flask, request, jsonify, render_template
-import pickle
 
-app = Flask(__name__,template_folder='/app')
-model = pickle.load(open('model_lr_ch1.pkl', 'rb'))
+
+app = Flask(__name__)
+
 
 @app.route('/')
 def home():
-    return render_template('/app/index.html')
+    return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
 
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+if __name__ == '__main__':
+    port = os.environ.get('FLASK_PORT') or 8080
+    port = int(port)
 
-    output = prediction[0]
-
-    return render_template('index.html', prediction_text='CHD would be  $ {}'.format(output))
-
-@app.route('/results',methods=['POST'])
-def results():
-
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output = prediction[0]
-    return jsonify(output)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=port,host='0.0.0.0')
